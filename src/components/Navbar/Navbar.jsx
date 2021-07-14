10f4014745b90cadfb400e279b5dd5eb3159c2d2
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-14 16:14:33 -0700

    added Store link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="link">
       TechSap
       <Link to="/">Home</Link>
-      
+      <Link to="/store">Store</Link>
     </nav>
   )
 }
