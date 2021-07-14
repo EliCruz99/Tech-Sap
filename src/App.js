a1a2f82bf2909b1d3843e760590a1fee54ab4bac
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-14 16:18:38 -0700

    added Store component and Route to Store

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Navbar from './components/Navbar/Navbar';
 import { Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Homepage from './components/Homepage';
-
+import Store from './components/Store/Store'
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Route exact path="/">
         <Homepage/>
+      </Route>
+      <Route path="/store">
+       <Store />
       </Route>
 
       <Footer/>
