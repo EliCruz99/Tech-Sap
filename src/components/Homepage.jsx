c9f2bde763e2bffc0ff83a23ddb4b7d058e289ee
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-14 09:04:25 -0700

    fixed margin to show 2 rows

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import axios from 'axios'
+import "./Homepage.css"
 import { useState, useEffect } from 'react'
 import { BASE_URL, headers } from '../Services'
 import ItemCard from './ItemCard/ItemCard'
+
+const pageSizeURL = `${BASE_URL}?pageSize=10`
 
 function Homepage() {
   const [items, setItems] = useState([])
@@ -12,7 +15,7 @@ function Homepage() {
   }, [])
 
   const fetchItems = async () => {
-    const res = await axios(BASE_URL, {headers})
+    const res = await axios(pageSizeURL, {headers})
     console.log(res.data.records)
     setItems(res.data.records)
   }
@@ -20,7 +23,7 @@ function Homepage() {
   
 
   return (
-    <div>
+    <div className='popular-items'>
       
       {items.map(item => {
         return <ItemCard key={item.id} item={item}/>
