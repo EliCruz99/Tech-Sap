7c88178698794bd267efaf05a18c2ce158e45a9a
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-16 11:05:22 -0700

    Added Linked and GitHub Icon links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import "./Footer.css"
 
 function Footer() {
   return (
     <footer>
+      <a href="https://github.com/EliCruz99" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faGithub} className="git" />
+      </a>
+      <a href="https://www.linkedin.com/in/elijah-cruz" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faLinkedin} className="linkedin" />
+       </a>
       
     </footer>
   )
