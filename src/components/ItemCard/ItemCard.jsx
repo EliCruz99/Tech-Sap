8399bcefd640cebeae1c4b75b31fe84130e90fa5
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-16 06:30:20 -0700

    Fixed linking issue I had

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 function ItemCard(props) {
   return (
     <div className="container">
-      <Link className="link" to={`${props.item.id}`}>
-      <img src={props.item.fields.imageURL} alt={props.item.fields.itemName}/>
+      <Link className="link" to={`/store/${props.item.id}`}>
+      <img className="image" src={props.item.fields.imageURL} alt={props.item.fields.itemName}/>
       <h2 className="name">{props.item.fields.itemName}</h2>
         <h4 className="price">{props.item.fields.price}</h4>
         </Link>
