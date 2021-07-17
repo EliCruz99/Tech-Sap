d3ae9706b275546ca46d8883bf42dee44fe01220
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-16 22:16:31 -0700

    Currently fixing styling issues but Pagination component works on the page

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { BASE_URL, headers } from '../../Services'
+import { BASE_URL, headers} from '../../Services'
 import ItemCard from '../ItemCard/ItemCard'
 import "./Store.css"
 import Pagination from '../Pagination/Pagination'
@@ -9,11 +9,8 @@ import Loader from '../Loader'
 
 
 
-
 function Store() {
   const [items, setItems] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage] = useState(10)
 
 
   useEffect(() => {
@@ -21,15 +18,11 @@ function Store() {
   }, [])
 
   const fetchItems = async () => {
-    const res = await axios(BASE_URL, {headers})
+    const res = await axios(BASE_URL, { headers })
     setItems(res.data.records)
   }
 
-  const lastSetOfItems = currentPage * itemsPerPage
-  const FirstSetOfItems = lastSetOfItems - itemsPerPage
-  const currentItems = items.slice(FirstSetOfItems, lastSetOfItems)
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  
   
   if (!items.fields) {
     <Loader/>
@@ -37,15 +30,11 @@ function Store() {
 
   return (
     <div className="items">
-      {currentItems.map(item => {
-        return (
-          <ItemCard key={item.id} item={item} />
-        )
-      })}
-      <Pagination
-        itemsPerPage={itemsPerPage}
-        totalItems={items.length}
-        paginate={paginate} />
+      <Pagination 
+      data={items}
+        RenderComponent={ItemCard}
+        pageLimit={5}
+        dataLimit={10}/>
     </div>
   )
 }
