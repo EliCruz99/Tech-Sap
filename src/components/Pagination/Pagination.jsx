20f24c569ec05bf48b580d29452728e1da97c48a
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-16 22:14:40 -0700

    Added pagination that allows you to go through pages of items

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,30 +1,81 @@
-import React from 'react'
 import "./Pagination.css"
-
-// Creates number Row that allows you to change item pages 
-
-function Pagination({ itemsPerPage, totalItems, paginate }) {
-  // create empty array 
+import { useState } from 'react'
 
 
-  let pageNum = []
-  // loop through totalItems divided by itemsPerPage *round up
-  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-    //push i into pageNum
-    pageNum.push(i)
+function Pagination({ data, RenderComponent, pageLimit, dataLimit }) {
+  const [pages] = useState(Math.round(data.length / dataLimit));
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function goToNextPage() {
+    setCurrentPage((page) => page + 1)
   }
 
+  function goToPreviousPage() {
+    setCurrentPage((page) => page - 1);
+  }
+
+  function changePage(event) {
+    const pageNumber = Number(event.target.textContent);
+    setCurrentPage(pageNumber);
+  }
+
+  const getPaginatedData = () => {
+    const startIndex = currentPage * dataLimit - dataLimit;
+    const endIndex = startIndex + dataLimit;
+    return data.slice(startIndex, endIndex);
+  };
+
+  const getPaginationGroup = () => {
+    let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+    return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+  };
+
   return (
-    <nav className="pagination">
-      {pageNum.map(number => (
-        <li key={number}>
-          <a onClick={() => paginate(number)} href="/store" className="numbers">
-            {number}
-          </a>
-        </li>
-  ))}
-    </nav>
-  )
+    <div>
+      {/* show the posts, 10 posts at a time */}
+      <div className="dataContainer">
+        {getPaginatedData().map((d, idx) => (
+          <RenderComponent key={idx} item={d} />
+        ))}
+      </div>
+  
+      {/* show the pagiantion
+          it consists of next and previous buttons
+          along with page numbers, in our case, 5 page
+          numbers at a time
+      */}
+      <div className="pagination">
+        {/* previous button */}
+        <button
+          onClick={goToPreviousPage}
+          className={`prev ${currentPage === 1 ? 'disabled' : ''}`}
+        >
+          prev
+        </button>
+  
+        {/* show page numbers */}
+        {getPaginationGroup().map((item, index) => (
+          <button
+            key={index}
+            onClick={changePage}
+            className={`paginationItem ${currentPage === item ? 'active' : null}`}
+          >
+            <span>{item}</span>
+          </button>
+        ))}
+  
+        {/* next button */}
+        <button
+          onClick={goToNextPage}
+          className={`next ${currentPage === pages ? 'disabled' : ''}`}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Pagination
+
+
