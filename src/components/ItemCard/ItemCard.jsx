f616b3a849a47ccfa4c71c6ddb962698a53b5a96
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-07-16 22:13:48 -0700

    fixed issue where cards weren't loading

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -6,9 +6,9 @@ function ItemCard(props) {
   return (
     <div className="container">
       <Link className="link" to={`/store/${props.item.id}`}>
-      <img className="image" src={props.item.fields.imageURL} alt={props.item.fields.itemName}/>
-      <h2 className="name">{props.item.fields.itemName}</h2>
-        <h4 className="price">{props.item.fields.price}</h4>
+      <img className="image" src={props.item.fields?.imageURL} alt={props.item.fields?.itemName}/>
+      <h2 className="name">{props.item.fields?.itemName}</h2>
+        <h4 className="price">{props.item.fields?.price}</h4>
         </Link>
       </div>
   )
